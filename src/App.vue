d96ccea0509fe805712fdae388d0f031dd908ba3
Luis Mariscal <luisferdev16@gmail.com>
2024-01-31 13:17:55 -0600

    starting project

---
+++ src/App.vue
@@ -0,0 +1,11 @@
+<script setup>
+import HeaderVue from './components/Header.vue';
+
+</script>
+
+<template>
+  <div class="my-20 max-w-lg mx-auto bg-white shadow p-10">
+    <HeaderVue/>
+  </div>
+</template>
+
