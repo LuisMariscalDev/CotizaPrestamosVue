df6fd15503fd9756a3ab90ac7fa37aaa692d1728
Luis Mariscal <luisferdev16@gmail.com>
2024-01-31 14:33:45 -0600

    range

--- src/App.vue
+++ src/App.vue
@@ -1,11 +1,27 @@
 <script setup>
-import HeaderVue from './components/Header.vue';
+  import { ref } from 'vue';
+  import Header from './components/Header.vue';
 
+  const cantidad = ref(0);
+
+  function handleChange(e) {
+    cantidad.value = Number(e.target.value);
+  }
 </script>
 
 <template>
   <div class="my-20 max-w-lg mx-auto bg-white shadow p-10">
-    <HeaderVue/>
+    <Header />
+
+    <div class="my-5">
+      <input
+        type="range"
+        class="w-full bg-gray-200 accent-lime-500 hover:accent-lime-600"
+        @input="handleChange"
+        />
+
+        {{cantidad}}
+    </div>
   </div>
 </template>
 
